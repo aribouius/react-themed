38d56bc8f76ef3415290a537daf36cd4ccaaa78a
Ari Bouius <abouius@rent.com>
2017-04-03 10:30:56 -0700

    Allow ThemeProvider to merge/compose themes

--- src/ThemeProvider-test.js
+++ src/ThemeProvider-test.js
@@ -1,7 +1,7 @@
 import React, { PropTypes } from 'react'
 import { expect } from 'chai'
 import { mount } from 'enzyme'
-import Provider from './ThemeProvider'
+import Theme from './ThemeProvider'
 
 describe('ThemeProvider', () => {
   const Foo = () => null
@@ -9,7 +9,68 @@ describe('ThemeProvider', () => {
 
   it('provides a `theme` context', () => {
     const theme = { foo: 'foo' }
-    const wrapper = mount(<Provider theme={theme} children={<Foo />} />)
+    const wrapper = mount(<Theme theme={theme} children={<Foo />} />)
+
     expect(wrapper.find(Foo).get(0).context).to.eql({ theme })
+  })
+
+  it('composes themes', () => {
+    const theme1 = { foo: 'foo' }
+    const theme2 = { foo: 'bar' }
+
+    const wrapper = mount(
+      <Theme theme={theme1}>
+        <Theme theme={theme2} compose>
+          <Foo />
+        </Theme>
+      </Theme>,
+    )
+
+    expect(wrapper.find(Foo).get(0).context.theme).to.eql({
+      foo: 'foo bar',
+    })
+  })
+
+  it('merges themes', () => {
+    const theme1 = { foo: 'foo', bar: 'bar' }
+    const theme2 = { foo: 'bar' }
+
+    const wrapper = mount(
+      <Theme theme={theme1}>
+        <Theme theme={theme2} merge>
+          <Foo />
+        </Theme>
+      </Theme>,
+    )
+
+    expect(wrapper.find(Foo).get(0).context.theme).to.eql({
+      foo: 'bar',
+      bar: 'bar',
+    })
+  })
+
+  it('merges themes using a custom merge function', () => {
+    const merge = (parent, child) => ({
+      ...parent,
+      ...child,
+      baz: 'baz',
+    })
+
+    const theme1 = { foo: 'foo' }
+    const theme2 = { bar: 'bar' }
+
+    const wrapper = mount(
+      <Theme theme={theme1}>
+        <Theme theme={theme2} merge={merge}>
+          <Foo />
+        </Theme>
+      </Theme>,
+    )
+
+    expect(wrapper.find(Foo).get(0).context.theme).to.eql({
+      foo: 'foo',
+      bar: 'bar',
+      baz: 'baz',
+    })
   })
 })
--- src/ThemeProvider.js
+++ src/ThemeProvider.js
@@ -1,17 +1,46 @@
 import { PureComponent, PropTypes, Children } from 'react'
+import deepMerge from 'lodash.merge'
+import composeTheme from './composeTheme'
 
 export default class ThemeProvider extends PureComponent {
+  static contextTypes = {
+    theme: PropTypes.object,
+  }
+
   static propTypes = {
     theme: PropTypes.object.isRequired,
+    merge: PropTypes.oneOfType([PropTypes.bool, PropTypes.func]),
+    compose: PropTypes.bool,
     children: PropTypes.element.isRequired,
   }
 
+  static defaultProps = {
+    merge: false,
+    compose: false,
+  }
+
   static childContextTypes = {
-    theme: PropTypes.object.isRequired,
+    theme: PropTypes.object,
   }
 
   getChildContext() {
-    return { theme: this.props.theme }
+    let theme = this.props.theme
+    const { merge, compose } = this.props
+    const parentTheme = this.context.theme
+
+    if (parentTheme) {
+      if (compose) {
+        theme = composeTheme(parentTheme, theme)
+      } else if (typeof merge === 'function') {
+        theme = merge(parentTheme, theme)
+      } else if (merge) {
+        theme = deepMerge({}, parentTheme, theme)
+      }
+    }
+
+    return {
+      theme,
+    }
   }
 
   render() {

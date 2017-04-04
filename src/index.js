dadc26bc88807623f68572ff2b55685e80f5e92d
Ari Bouius <abouius@rent.com>
2017-04-04 11:09:54 -0700

    Export Theme alias for ThemeProvider

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export { default as themed } from './themed'
-export { default as themeContext } from './themeContext'
+export { default as Theme } from './ThemeProvider'
 export { default as ThemeProvider } from './ThemeProvider'
+export { default as themeContext } from './themeContext'
 export { default as composeTheme } from './composeTheme'
